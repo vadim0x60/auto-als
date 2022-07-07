8ad3f649e130b03434e098a9a106698c71c1866b
Bhalwankar <Rajesh.Bhalwankar@philips.com>
2022-07-07 15:58:03 +0200

    Builds path folder issue resolved: path set to Path.cwd()/UnityBuilds

--- auto_als/envs.py
+++ auto_als/envs.py
@@ -17,7 +17,7 @@ from tenacity import retry
 from tenacity.wait import wait_exponential
 from tenacity.retry import retry_if_exception_type
 
-BUILDS_PATH = Path(__name__).parent / 'UnityBuilds'
+BUILDS_PATH = Path.cwd() / 'UnityBuilds'
 
 virtu_als_release = '1.0'
 
