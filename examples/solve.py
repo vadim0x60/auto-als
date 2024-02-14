082df2489c7d482b2c14520b068e4fd96cdd05d2
Vadim Liventsev <dev@vadim.me>
2024-02-14 16:20:19 +0100

    Added wandb logging to example

--- examples/solve.py
+++ examples/solve.py
@@ -3,16 +3,44 @@ import click
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.callbacks import CheckpointCallback
+
+import wandb
+from wandb.integration.sb3 import WandbCallback
 
 import gym
 import auto_als
 
+TOTAL_TIMESTEPS=10000
+
+def no_history_obs(obs):
+    obs = obs.copy()
+    obs[:43][obs[:43] < 1] = 0
+    return obs
+
 @click.command()
 @click.option('--attach/--launch', default= False)
-def main(attach):
+@click.option('--baseline', is_flag=True)
+def main(attach, baseline):
     #evaluate_policy(model, env)
     env = gym.make('Auto-ALS-v0', attach=attach, render = True)
+    env = gym.wrappers.TimeLimit(env, max_episode_steps=256)
+
+    config = {
+        'total_timesteps': TOTAL_TIMESTEPS,
+        'env': 'Auto-ALS-v0',
+        'baseline': baseline
+    }
+
+    run = wandb.init(
+        project="auto-als",
+        config=config,
+        sync_tensorboard=True,
+        monitor_gym=True,
+        save_code=True
+    )
+
+    if baseline:
+        env = gym.wrappers.TransformObservation(env, no_history_obs)
 
     print('First episode is a sanity check. Let us do something random')
     env.reset()
@@ -32,8 +60,11 @@ def main(attach):
     env.reset()
     print('Sanity check OK, looking for optimal solution')
 
-    model = PPO('MlpPolicy', env, verbose=True).learn(10000, callback=CheckpointCallback(1000, 'PPO'))
+    alg = PPO('MlpPolicy', env, verbose=1, tensorboard_log=f"runs/{run.id}")
+    model = alg.learn(TOTAL_TIMESTEPS, callback=WandbCallback(model_save_freq=100, gradient_save_freq=100, model_save_path=f"models/{run.id}", verbose=2))
     evaluate_policy(model, env)
+
+    run.finish()
 
 if __name__ == '__main__':
     main()
