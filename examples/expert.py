56eb319db145982691b276051f09241ab690f014
Vadim Liventsev <dev@vadim.me>
2024-03-04 16:35:16 +0100

    Fix

--- examples/expert.py
+++ examples/expert.py
@@ -9,7 +9,7 @@ def master(render):
 
     env = gym.make('Auto-ALS-v0', attach=False, render=render, autoplay=True)
     for _ in range(1 if render else 3):
-        env.reset()
+        print(env.reset())
         print(env.step(actions['AssessAirway']))
         print(env.step(actions['AssessBreathing']))
         print(env.step(actions['AssessCirculation']))
