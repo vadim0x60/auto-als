3a9a2a017ecd03b3d6dd6ae3acaf9acdd78770a7
Vadim Liventsev <dev@vadim.me>
2024-03-08 13:58:08 +0100

    model runner

---
+++ examples/run_model.py
@@ -0,0 +1,18 @@
+import click
+from stable_baselines3.common.base_class import BaseAlgorithm 
+import gymnasium as gym
+
+@click.command()
+@click.argument('path')
+def run_model(path):
+    model = BaseAlgorithm.load(path)
+    env = gym.make('Auto-ALS-v0', attach=False, render=False, autoplay=True)
+
+    obs, info = env.reset()
+    terminated = False
+    truncated = False
+    while not (terminated or truncated):
+        action = model.predict(obs, deterministic=True)
+        _, reward, terminated, truncated, info = env.step(action)
+        print(info['memos'])
+        print(reward)
