7d3a7d418e5890da5188c1e0f0b6533692fc42eb
Vadim Liventsev <dev@vadim.me>
2024-03-31 17:32:18 +0200

    reset bomb test

--- examples/actions.py
+++ examples/actions.py
@@ -2,13 +2,20 @@ import gymnasium as gym
 import auto_als
 import click
 
+reset_hint = """
+Number of times to reset the environment
+Reset bombing has shown to be a useful stress test for the environment
+"""
+
 @click.command('Try all available actions')
 @click.option('--render', is_flag=True)
-def actions(render):
-    actions = {a: i for i, a in enumerate(auto_als.actions)}
-
+@click.option('--reset-count', default=1, type=int, help=reset_hint)
+def actions(render, reset_count):
     env = gym.make('Auto-ALS-v0', attach=False, render=render, autoplay=True)
-    print(env.reset())
+
+    for _ in range(reset_count):
+        print(env.reset())
+
     for idx, name in enumerate(auto_als.actions):
         print(name)
         ret = env.step(idx)
