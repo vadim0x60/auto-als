935b82baef15bc1eb075b21dcf9f6fc2496d4392
Vadim Liventsev <dev@vadim.me>
2024-04-04 22:26:54 +0200

    fix

--- auto_als/envs.py
+++ auto_als/envs.py
@@ -121,12 +121,6 @@ class AutoALS(gym.Env, SideChannel):
             self.unity_env = proivision_unity_env(self.render_, self.attach_, self.autoplay_, [self], 
                                                   log_folder=self.log_folder)
             self.rl_env = UnityToGymWrapper(self.unity_env)
-
-            obs_str = f'UnityToGymWrapper has observation space {self.rl_env.observation_space.shape}, {self.observation_space.shape} expected'
-            act_str = f'UnityToGymWrapper has action space {self.rl_env.action_space.n}, {self.action_space.n} expected'
-
-            assert self.rl_env.observation_space.shape == self.observation_space.shape, obs_str
-            assert self.rl_env.action_space.n == self.action_space.n, act_str
         except (UnityException, UnityGymException) as e:
             raise AutoALSException('Unity environment is not starting as expected') from e
         
